5c8fa892a9090f19dca4bf52678063b2eb96be65
HosseinJoonz <64345781+XxHosseinJoonZxX@users.noreply.github.com>
2022-12-08 16:14:54 +0330

    main.py
    Main File
    
    Signed-off-by: HosseinJoonz <64345781+XxHosseinJoonZxX@users.noreply.github.com>

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ app_window.title("Digital Clock")
 app_window.geometry("420x150") 
 app_window.resizable(1,1)
 
+
 text_font= ("Boulder", 68, 'bold')
 background = "#f2e750"
 foreground= "#363529"
 border_width = 25
 
+
 label = Label(app_window, font=text_font, bg=background, fg=foreground, bd=border_width) 
 label.grid(row=0, column=1)
+
 
 def digital_clock(): 
     time_live = time.strftime("%H:%M:%S")
@@ -20,4 +23,5 @@ def digital_clock():
 
 digital_clock()
 app_window.mainloop()
+
 
